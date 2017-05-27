473a63f6e347308f64886d4311297373356ab7b2
derekpastor <derekjonp@gmail.com>
2017-05-27 13:02:49 -0400

    setting up auth

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+var config = {
+  apiKey: "AIzaSyDp3FFbviWq7Z-IhECOGdqY_ezzAj3197I",
+  authDomain: "brigade-929f9.firebaseapp.com",
+  databaseURL: "https://brigade-929f9.firebaseio.com",
+  projectId: "brigade-929f9",
+  storageBucket: "brigade-929f9.appspot.com",
+  messagingSenderId: "54936219962"
+};
