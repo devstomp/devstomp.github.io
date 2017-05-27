a4d4d8cb0510697db4718570f5e9855288353d82
derekpastor <derekjonp@gmail.com>
2017-05-27 14:01:09 -0400

    finshed login

--- scripts.js
+++ scripts.js
@@ -38,6 +38,7 @@ function signingithub(){
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
+  alert(error);
   console.log(error);
   var errorMessage = error.message;
   // The email of the user's account used.
