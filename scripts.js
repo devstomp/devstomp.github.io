c4c9894b1ee8fa02d3d8105eea66a7faf5d78185
derekpastor <derekjonp@gmail.com>
2017-05-27 14:27:58 -0400

    correct login and main page

--- scripts.js
+++ scripts.js
@@ -48,3 +48,11 @@ function signingithub(){
   // ...
 });
 }
+function loginButton() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
