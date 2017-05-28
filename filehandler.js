f803f6729b15a7f40a1df42e30e35cff2870fa63
derekpastor <derekjonp@gmail.com>
2017-05-28 03:42:42 -0400

    finshed the make a post function

--- filehandler.js
+++ filehandler.js
@@ -70,6 +70,65 @@ console.log(e);
      updates['users/'+authuser.uid+'/'+newPostKey] = postData;
      return firebase.database().ref().update(updates);
     }
+    function displayPost(username, comments, filebin,type, snippet, mainC, time ){
+      var outline = document.createElement("div");
+      var inline= document.createElement("div");
+      var main = document.createElement("p");
+      var filesdownload = document.createElement("div");
+      var commentsection = document.createElement("div");
+      var load= document.createElement("div");
+      var loadmore = document.createElement("button");
+      var snippetSpace= document.createElement("div");
+      inline.className="content-container";
+      outline.className = "content-card";
+      inline.style="margin:0%";
+      load.style="text-align:center";
+      loadmore.style="width:100%";
+      loadmore.className="btn btn-default";
+      main.innerHTML= username + ": "+mainC+" - "+type+" --- "+time;
+      makeComment(comments[0].Author, comments[0].ProfilePic, comments[0].Words, commentsection);
+      loadmore.onclick = function(){
+        while (commentsection.hasChildNodes()) {
+              commentsection.removeChild(commentsection.lastChild);
+          }
+        for(var i=0;i<comments.length; i++){
+          makeComment(comments[i].Author, comments[i].ProfilePic, comments[i].Words, commentsection);
+        }
+        load.removeChild(loadmore);
+      }
+      if(snippet!=""){
+        createSnippet(snippet,snippetSpace);
+      }
+      for(var i=0; i<filebin.length;i++){
+        storageRef.child(filebin[i]).getDownloadURL().then(function(url) {
+          makeFileDownload(getSecondPart(filebin[i]), url,filesdownload);
+      });
+      }
+      inline.appendChild(main);
+      inline.appendChild(snippetSpace);
+      inline.appendChild(filesdownload);
+      inline.appendChild(commentsection);
+      inline.appendChild(load);
+      load.appendChild(loadmore);
+      outline.appendChild(inline);
+      document.getElementById("Content").appendChild(outline);
+
+
+
+    }
+    function getSecondPart(str) {
+    return str.split('/')[1];
+}
+
+function createSnippet(snip, dev){
+
+
+
+var temp = document.createElement('div');
+temp.innerHTML = snip;
+var htmlObject = temp.firstChild;
+dev.appendChild(htmlObject);
+}
     function makeComment(name, img, text, dev){
       var div = document.createElement("div");
       div.className = "comments";
@@ -89,11 +148,24 @@ console.log(e);
       div.appendChild(speech);
       dev.appendChild(div);
     }
+    function writeComment(writing){
+      var d = new Date();
+
+      var comment={
+        Author:authuser.displayName,
+        Time:d.getTime(),
+        ProfilePic:authuser.photoURL,
+        Words, writing
+      };
+
+    }
   function  makeFileDownload(finame, downloadurl, dev){
     var filename = finame;
     var a  = document.createElement("a");
     var button= document.createElement("button");
     var text = document.createElement("p");
+    var hr = document.createElement("hr");
+
     text.innerHTML = filename;
     text.style="display:inline-block;"
     button.className = "btn btn-primary";
@@ -102,7 +174,9 @@ console.log(e);
     a.download = filename;
     a.style="display:inline-block;";
     a.appendChild(button);
+    dev.appendChild(hr);
     dev.appendChild(text);
     dev.appendChild(a);
+    dev.appendChild(hr);
 
   }
