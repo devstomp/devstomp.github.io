dc1e7795408d22d8f84f641e7aab297c5f2990ee
derekpastor <derekjonp@gmail.com>
2017-05-28 14:40:44 -0400

    fixed funny error

--- filehandler.js
+++ filehandler.js
@@ -129,7 +129,7 @@ function displayPost(key,username, comments, filebin,type, snippet, mainC, time 
       for(var i=0; i<filebin.length;i++){
         storageRef.child(filebin[i]).getDownloadURL().then(function(url) {
           console.log(filebin);
-          makeFileDownload(filebin[i][0], url,filesdownload);
+          makeFileDownload(filebin[i], url,filesdownload);
       });
       }
       inline.appendChild(main);
