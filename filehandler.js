4928c980940b870d62d94f42bf3903519b199d3c
derekpastor <derekjonp@gmail.com>
2017-05-27 20:03:33 -0400

    fixed small error with pointer

--- filehandler.js
+++ filehandler.js
@@ -4,7 +4,44 @@ var index=0;
 var fileplacing = $(".Uploads")
 var filelists=[];
 
+function initApp() {
+  // Listening for auth state changes.
+  // [START authstatelistener]
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
 
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      console.log(displayName);
+      window.location.href = "home.html";
+      // [START_EXCLUDE]
+  //    document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+    //  document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+      //document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+      // [END_EXCLUDE]
+    } else {
+      // User is signed out.
+      // [START_EXCLUDE]
+    //  document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+      //document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
+      //document.getElementById('quickstart-account-details').textContent = 'null';
+      //document.getElementById('quickstart-oauthtoken').textContent = 'null';
+      // [END_EXCLUDE]
+    }
+    // [START_EXCLUDE]
+    //document.getElementById('quickstart-sign-in').disabled = false;
+    // [END_EXCLUDE]
+  });
+  // [END authstatelistener]
+  //document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+}
+     
 $(document).on('change', fileinput, function(e){
 
  var input = e.target;
