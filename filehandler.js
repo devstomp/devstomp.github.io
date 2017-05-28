27611e086d679892fcd2e099a1cec7b31364cb27
derekpastor <derekjonp@gmail.com>
2017-05-28 00:50:01 -0400

    after much debate it is finshed

--- filehandler.js
+++ filehandler.js
@@ -1,19 +1,20 @@
 var fileinput = $("input:file");
 var index=0;
-var fileplacing = $(".Uploads")
+var fileplacing = $(".Uploads");
+var snippet;
+var maincomment;
 var filelists=[];
 var authuser;
 function initApp() {
+  snippet =document.getElementById("htmlsnippet");
+  maincomment = document.getElementById("comment");
   firebase.auth().onAuthStateChanged(function(user) {
+
     if (user) {
       authuser = user;
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
+
+
+
       console.log(displayName);
     } else {
     //    window.location.href = "login.html";
@@ -34,19 +35,20 @@ $(document).on('change', fileinput, function(e){
         fileplace.appendChild(div);
 
     console.log(input.files);
-    console.log(filelists[0][0].name);
+
   });
 
 function post(){
   var database = firebase.database();
+
+  alert("snippet" + snippet + "    main comment" + maincomment);
   var d = new Date();
     var metadata = {
       'Author': authuser.email,
     };
   var timenow = d.getTime();
   var filepath=[];
-  var snippet = $("#htmlsnippet").value;
-  var maincomment = $("#comment").value;
+
   var storageRef = firebase.storage().ref();
       for (var i = 0; i < filelists.length; i++) {
       try{  filepath.push(authuser.email+'/'+ filelists[i][0].name);
@@ -63,10 +65,10 @@ console.log(e);
     var postData = {
       "Time": timenow,
       "Author": authuser.email,
-      "MainComment":maincomment,
+      "MainComment":maincomment.value,
       "FilePath":filepath,
       "Type":"HTML",
-      "Snippet":snippet,
+      "Snippet":snippet.value,
       "Comments":comments
     };
 
