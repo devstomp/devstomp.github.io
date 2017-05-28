76d48e26cab2a32aea377244b139e5821be273d8
derekpastor <derekjonp@gmail.com>
2017-05-27 20:51:11 -0400

    fixed github api

--- github.js
+++ github.js
@@ -1,15 +1,12 @@
 Query.githubUser = function(username, callback) {
   jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
 }
-
 jQuery.fn.loadRepositories = function(username) {
   this.html("<span>Querying GitHub for repositories...</span>");
-
   var target = this;
   $.githubUser(username, function(data) {
     var repos = data.user.repositories;
     sortByNumberOfWatchers(repos);
-
     var list = $('<dl/>');
     target.empty().append(list);
     $(repos).each(function() {
@@ -17,7 +14,6 @@ jQuery.fn.loadRepositories = function(username) {
       list.append('<dd>' + this.description + '</dd>');
     });
   });
-
   function sortByNumberOfWatchers(repos) {
     repos.sort(function(a,b) {
       return b.watchers - a.watchers;
