7a0ae4a437fc504dee3a9a099c2ce776b59b440a
derekpastor <derekjonp@gmail.com>
2017-05-28 04:23:31 -0400

    fixed

--- filehandler.js
+++ filehandler.js
@@ -23,7 +23,7 @@ function contentMain(){
   htmlRef.orderByKey().on('value', function(snapshot){
     console.log(snapshot.val());
       snapshot.forEach(function(childSnapshot) {
-          console.log(childSnapshot.val());
+          console.log(childSnapshot.val().Author);
         displayPost(childSnapshot.key,childSnapshot.val().Author,childSnapshot.val().Comments,childSnapshot.val().FilePath,childSnapshot.val().Type, childSnapshot.val().Snippet,  childSnapshot.val().MainComment, childSnapshot.val().Time);
       });
   });
