a09f27e4362d4558da909aba0f1c47d5d69d8beb
derekpastor <derekjonp@gmail.com>
2017-05-28 02:40:20 -0400

    made first post example

--- filehandler.js
+++ filehandler.js
@@ -9,23 +9,16 @@ function initApp() {
   snippet =document.getElementById("htmlsnippet");
   maincomment = document.getElementById("comment");
   firebase.auth().onAuthStateChanged(function(user) {
-
     if (user) {
       authuser = user;
-
-
-
-  console.log(authuser.uid);
     } else {
-    //    window.location.href = "login.html";
+      // window.location.href = "login.html";
     }
   });
 }
 $(document).on('change', fileinput, function(e){
   var input = e.target;
   filelists.push(input.files);
-
-
         var div = document.createElement("div");
         div.className = "well well-sm";
         try{div.innerHTML = e.target.files[0].name;
@@ -33,7 +26,6 @@ $(document).on('change', fileinput, function(e){
           console.log(e);
         }
         fileplace.appendChild(div);
-
     console.log(input.files);
 
   });
@@ -77,4 +69,23 @@ console.log(e);
      updates['HTML/' + newPostKey] = postData;
      updates['users/'+authuser.uid+'/'+newPostKey] = postData;
      return firebase.database().ref().update(updates);
+    }
+    function makeComment(name, img, text, dev){
+      var div = document.createElement("div");
+      div.className = "comments";
+      var image= document.createElement("img");
+      var fname = document.createElement("p");
+      var speach =document.createElement("p");
+      var breaking = document.createElement("br");
+      fname.innerHTML = name;
+      speach.innerHTML = text;
+      image.src=img;
+      image.className = "img-rounded";
+      fname.style = "display:inline-block;font-size:60%;"
+      speach.style = "font-size:40%;";
+      div.appendChild(image);
+      div.appendChild(fname);
+      div.appendChild(breaking);
+      div.appendChild(speech);
+      dev.appendChild(div);
     }
