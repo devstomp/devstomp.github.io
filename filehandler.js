a633d3ad28673b3d7171a8c42141fbe036ec9297
derekpastor <derekjonp@gmail.com>
2017-05-27 20:09:07 -0400

    fixed error oppsssss

--- filehandler.js
+++ filehandler.js
@@ -19,13 +19,14 @@ function initApp() {
       var uid = user.uid;
       var providerData = user.providerData;
       console.log(displayName);
-      window.location.href = "home.html";
+
       // [START_EXCLUDE]
   //    document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
     //  document.getElementById('quickstart-sign-in').textContent = 'Sign out';
       //document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
       // [END_EXCLUDE]
     } else {
+        window.location.href = "login.html";
       // User is signed out.
       // [START_EXCLUDE]
     //  document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
@@ -41,13 +42,14 @@ function initApp() {
   // [END authstatelistener]
   //document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
 }
-     
+
+
 $(document).on('change', fileinput, function(e){
 
  var input = e.target;
 filelists.push(input.files);
 //files.push(input.files)
-var storageRef = firebase.storage().ref();
+
 
     for (var i = 0; i < input.files.length; i++) {
 
@@ -62,12 +64,14 @@ var storageRef = firebase.storage().ref();
 console.log(filelists);
   });
 function post(){
-
+  var metadata = {
+    author: user.email,
+  };
   var storageRef = firebase.storage().ref();
 
       for (var i = 0; i < filelists.length; i++) {
   var Ref = storageRef.child(filelists[i][0].name);
-  Ref.put(filelists[i][0]).then(function(snapshot) {
+  Ref.put(filelists[i][0], metadata).then(function(snapshot) {
     console.log("Uploaded");
   });
 }
