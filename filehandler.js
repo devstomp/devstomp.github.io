c68aca98e64b20568c4cca74c741dc5c685da0ac
derekpastor <derekjonp@gmail.com>
2017-05-27 20:33:42 -0400

    cleaned up some code

--- filehandler.js
+++ filehandler.js
@@ -5,12 +5,9 @@ var fileplacing = $(".Uploads")
 var filelists=[];
 var authuser;
 function initApp() {
-  // Listening for auth state changes.
-  // [START authstatelistener]
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-authuser = user;
-      // User is signed in.
+      authuser = user;
       var displayName = user.displayName;
       var email = user.email;
       var emailVerified = user.emailVerified;
@@ -19,45 +16,19 @@ authuser = user;
       var uid = user.uid;
       var providerData = user.providerData;
       console.log(displayName);
-
-      // [START_EXCLUDE]
-  //    document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-    //  document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-      //document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-      // [END_EXCLUDE]
     } else {
         window.location.href = "login.html";
-      // User is signed out.
-      // [START_EXCLUDE]
-    //  document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-      //document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
-      //document.getElementById('quickstart-account-details').textContent = 'null';
-      //document.getElementById('quickstart-oauthtoken').textContent = 'null';
-      // [END_EXCLUDE]
     }
-    // [START_EXCLUDE]
-    //document.getElementById('quickstart-sign-in').disabled = false;
-    // [END_EXCLUDE]
   });
-  // [END authstatelistener]
-  //document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
 }
-
-
 $(document).on('change', fileinput, function(e){
-
  var input = e.target;
 filelists.push(input.files);
-//files.push(input.files)
-
-
     for (var i = 0; i < input.files.length; i++) {
-
           console.log(input.files[i].name);
           var div = document.createElement("div");
           div.className = "well well-sm";
           div.innerHTML = e.target.files[i].name;
-
           fileplace.appendChild(div);
         }
     console.log(input.files);
@@ -68,15 +39,10 @@ function post(){
     Author: authuser.email,
   };
   var storageRef = firebase.storage().ref();
-
       for (var i = 0; i < filelists.length; i++) {
-  var Ref = storageRef.child(filelists[i][0].name);
-  Ref.put(filelists[i][0], metadata).then(function(snapshot) {
-    console.log("Uploaded");
-  });
-}
-
-
-
-
+        var Ref = storageRef.child(filelists[i][0].name);
+          Ref.put(filelists[i][0], metadata).then(function(snapshot) {
+              console.log("Uploaded");
+          });
+      }
     }
--- scripts.js
+++ scripts.js
@@ -12,20 +12,14 @@ var config = {
 function signingoogle(){
   var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
-  // The signed-in user info.
    user = result.user;
-
 }).catch(function(error) {
-  // Handle Errors here.
+
   var errorCode = error.code;
   var errorMessage = error.message;
-  // The email of the user's account used.
   var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
   var credential = error.credential;
-  // ...
 });
 
 initApp();
@@ -33,23 +27,16 @@ initApp();
 function signingithub(){
   var provider = new firebase.auth.GithubAuthProvider();
   firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
   var token = result.credential.accessToken;
-  // The signed-in user info.
  user = result.user;
   console.log(user);
-  // ...
 }).catch(function(error) {
-  // Handle Errors here.
   var errorCode = error.code;
   alert(error);
   console.log(error);
   var errorMessage = error.message;
-  // The email of the user's account used.
   var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
   var credential = error.credential;
-  // ...
 });
 initApp();
 }
@@ -62,12 +49,8 @@ function loginButton() {
     }
 }
 function initApp() {
-  // Listening for auth state changes.
-  // [START authstatelistener]
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-
-      // User is signed in.
       var displayName = user.displayName;
       var email = user.email;
       var emailVerified = user.emailVerified;
@@ -77,24 +60,7 @@ function initApp() {
       var providerData = user.providerData;
       console.log(displayName);
       window.location.href = "home.html";
-      // [START_EXCLUDE]
-  //    document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-    //  document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-      //document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-      // [END_EXCLUDE]
-    } else {
-      // User is signed out.
-      // [START_EXCLUDE]
-    //  document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-      //document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
-      //document.getElementById('quickstart-account-details').textContent = 'null';
-      //document.getElementById('quickstart-oauthtoken').textContent = 'null';
-      // [END_EXCLUDE]
+        } else {
     }
-    // [START_EXCLUDE]
-    //document.getElementById('quickstart-sign-in').disabled = false;
-    // [END_EXCLUDE]
   });
-  // [END authstatelistener]
-  //document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
 }
