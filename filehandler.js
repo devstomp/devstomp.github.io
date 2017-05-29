407ba1312d36207ac313545af5a06d45cf186464
derekpastor <derekjonp@gmail.com>
2017-05-28 21:29:20 -0400

    new post data indendentiy

--- filehandler.js
+++ filehandler.js
@@ -104,8 +104,11 @@ function displayPost(key,username, comments, filebin,type, snippet, mainC, time 
       loadmore.innerHTML = "Load more";
       loadmore.className="btn btn-default";
       main.innerHTML= username+":"+mainC+" - "+type;
-      if(comments!=[['','']])
-      makeComment(key,comments.Author, comments.ProfilePic, comments.Words, commentsection);
+console.log(comments);
+      if(comments!=[['','']]){
+        makeComment(key,comments.Author, comments.ProfilePic, comments.Words, commentsection);
+
+      }
 
         var storageRef = firebase.storage().ref();
       loadmore.onclick = function(){
@@ -113,9 +116,11 @@ function displayPost(key,username, comments, filebin,type, snippet, mainC, time 
               commentsection.removeChild(commentsection.lastChild);
           }
       //  for(var i=0;i<comments.length; i++){
-      if(comments!=[['','']])
+      if(comments!=[['','']]){
+        makeComment(key,comments.Author, comments.ProfilePic, comments.Words, commentsection);
 
-          makeComment(key,comments.Author, comments.ProfilePic, comments.Words, commentsection);
+      }
+
         //}
         load.removeChild(loadmore);
         load.appendChild(newComment);
